7ab91af2975513803971f8c301971276e210768b
Sabby Anandan <sabby.anandan@gmail.com>
2018-01-21 09:37:56 -0800

    No need to deploy yet

--- fooz/build-project.sh
+++ fooz/build-project.sh
@@ -9,5 +9,5 @@ cleanup_maven_repo "com.exmaple"
 repository=$(pwd)/distribution-repository
 
 pushd the-gitrigger > /dev/null
-run_maven -f pom.xml clean deploy -U -Dfull -DaltDeploymentRepository=distribution::default::file://${repository}
+run_maven -f pom.xml clean install -U -Dfull -DaltDeploymentRepository=distribution::default::file://${repository}
 popd > /dev/null
