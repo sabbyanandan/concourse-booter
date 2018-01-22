8de4a22dd318bd87a3677b21381ec2aea2816825
Sabby Anandan <sabby.anandan@gmail.com>
2018-01-22 12:31:01 -0800

    try my luck

--- ci/build-project.sh
+++ ci/build-project.sh
@@ -4,5 +4,7 @@ set -e -u
 
 [[ -d $PWD/maven && ! -d $HOME/.m2 ]] && ln -s $PWD/maven $HOME/.m2
 
+repository=$(pwd)/distribution-repository
+
 cd concourse-booter
-./mvnw deploy -DaltDeploymentRepository=distribution::default::file://$(pwd)/distribution-repository
+./mvnw deploy -DaltDeploymentRepository=distribution::default::file://${repository}
