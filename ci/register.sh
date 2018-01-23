f6adce2a88b074b39b6f1f23caf2e7fe094fbe46
Sabby Anandan <sabby.anandan@gmail.com>
2018-01-23 15:24:10 -0800

    Add curl

--- ci/register.sh
+++ ci/register.sh
@@ -4,14 +4,9 @@ set -e -u
 
 VERSION=$(find distribution-repository -name "*.jar" | xargs basename | sed 's|concourse-booter-\(.*\)\.jar|\1|')
 
-echo $VERSION
+echo "Registering concourse-booter ${VERSION}"
 
-# echo "Registering ${buildName}/${buildNumber}"
-
-# curl \
-# 	-X \
-# 	POST "http://sabby-test-dataflow-server.cfapps.io/apps/source/foo" > /dev/null || { echo "Failed to promote" >&2; exit 1; }
-#     -d "uri=maven://com.example:concourse-boot-sample:0.0.1"
-
-
-# echo "Registered ${buildName}/${buildNumber}"
+curl \
+	-X \
+	POST "http://sabby-test-dataflow-server.cfapps.io/apps/source/concourse-booter" > /dev/null || { echo "Failed to promote" >&2; exit 1; }
+    -d "uri=maven://com.example:concourse-booter:${VERSION}"
